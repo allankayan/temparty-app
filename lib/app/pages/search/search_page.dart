e7dc3a1b7e048f305c8cbfd941960a0429db1a56
Allan <4llankayan@gmail.com>
2022-11-16 01:05:55 -0300

    change old layout

--- lib/app/pages/search/search_page.dart
+++ lib/app/pages/search/search_page.dart
@@ -1,4 +1,6 @@
+import 'package:draggable_home/draggable_home.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 import 'package:temparty/app/pages/search/search_controller.dart';
 import 'package:flutter/material.dart';
 
@@ -14,43 +16,59 @@ class SearchPageState extends State<SearchPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.only(top: 40, bottom: 20),
-              child: Image(
-                width: 140,
-                image: AssetImage('assets/images/temparty.png'),
-                fit: BoxFit.contain,
-              ),
-            ),
-            SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  mainAxisSize: MainAxisSize.max,
-                  children: const [
-                    Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        'PROCURAR EVENTOS',
-                        style: TextStyle(
-                          fontSize: 22,
-                          color: Colors.deepPurple,
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+    return DraggableHome(
+      title: Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: GradientText(
+          'Temparty',
+          style: const TextStyle(
+            fontSize: 20,
+            color: Colors.deepPurple,
+            fontWeight: FontWeight.w800,
+            letterSpacing: 2,
+          ),
+          colors: const [
+            Colors.purpleAccent,
+            Colors.deepPurpleAccent,
           ],
+        ),
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.deepPurpleAccent,
+            ),
+          ),
+        ),
+      ],
+      centerTitle: false,
+      curvedBodyRadius: 8,
+      headerExpandedHeight: 0.20,
+      fullyStretchable: false,
+      backgroundColor: Colors.white,
+      appBarColor: Colors.white,
+      headerWidget: headerWidget(context),
+      body: [],
+    );
+  }
+
+  Widget headerWidget(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/background.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: const Center(
+        child: Image(
+          width: 140,
+          image: AssetImage('assets/images/temparty.png'),
+          fit: BoxFit.contain,
         ),
       ),
     );
